2e1e23579f4e7f99d3908b28b736534177ad3854
Vitaly Simakov <faetonvs@gmail.com>
2019-01-30 17:31:32 +0500

    open users by id

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  resources :job_managers
-  #get "/job/:id/start" => "job_managers#job_start", as: :job_start
-  #get "/job/:id/stop" => "job_managers#job_stop", as: :job_stop
-  #get '/job' => "job_managers#index"
-  #get '/job/run' => "job_managers#new"
-  #post '/job/run' => "job_managers#new"
-  get 'home_pages/Index'
-
   devise_for :users
   resources :short_urls
 
@@ -15,10 +7,10 @@ Rails.application.routes.draw do
   match "/user/:id/url/:short_url" => "users#open", via: :get, as: :go_on_url
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # get "/users" => "users#index"
+  get "/users" => "users#index"
   get "/user/:id" => "users#show", as: :user
 
   get "/:short_url" => "short_urls#open"
 
-  root "home_pages#Index"
+  root "short_urls#index"
 end
