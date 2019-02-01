a525cd77f4db3a91dc794970487ef6d0265371a5
Vitaly Simakov <faetonvs@gmail.com>
2019-02-01 16:09:51 +0500

    changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  #def is_url_owner?(user_signed_in, current_user)
+  def is_url_owner?(short_url)
+    user_signed_in? and short_url.user_id == current_user.id
+  end
 end
--- app/helpers/short_urls_helper.rb
+++ app/helpers/short_urls_helper.rb
@@ -1,5 +1,3 @@
 module ShortUrlsHelper
-  def is_url_owner?(user_signed_in, current_user)
-    user_signed_in and user_id == current_user.id
-  end
+
 end
